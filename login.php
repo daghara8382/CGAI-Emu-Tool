cffe45972a20637cb7dc11851f0caccb33632730
daghara8382 <114475485+daghara8382@users.noreply.github.com>
2024-01-25 21:23:23 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,48 +3,21 @@
 include 'includes/config/_config.php';
 function setLoginwwid($wwid)
 {
-	$redirect_url = isset($_GET['redirect']) && !empty($_GET['redirect']) ? $_GET['redirect'] : '';
+	
 	$db = new Db();
 	
 	$sql = "SELECT Idsid,WWID FROM `Recognition`.`hspe_employees` WHERE `WWID`='$wwid'";
 	$readuser	=  $db->select($sql);
 	$idsid = $readuser[0]['Idsid'];
-	$pass = 0;
-	$id = "";
-	$msg = "";
+	
 
 	if ($readuser[0]['WWID'] === $wwid) {
 		$pass	=	1;
 		$id 	=	$idsid;
-	} else {
-		/* if(!insertNewSubscribers($idsid)){
-			require_once "templates/home.php";
-			exit();
-		}else{
-			$pass	=	1;
-			$id 	=	$idsid; //BB-VALID cookie id
-		} */
-		setcookie('IDSID', $idsid, time() + (86400 * 30), "/");
-		require_once "templates/home.php";
-		exit();
-		/* if(!is_bb($idsid)){
-			require_once "templates/401.php";
-			exit();
-		}else{
-			$pass	=	1;
-			$id 	=	$idsid; //BB-VALID cookie id
-		} */
-	}
+	} 
 	if ($pass == 1) {
 		setcookie('IDSID', $id, time() + (86400 * 30), "/");
 		$_SESSION['id'] = $id;
-		if (!empty($redirect_url)) {
-			$redirect_url1 = urldecode($redirect_url);
-			header("location:$redirect_url1");
-		} else {
-
-			header("location:imet_home.php");
-		}
 	}
 }
 <html lang="en">
